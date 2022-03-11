5c1f89d12986caa3ec1404ca49d8588a7ca44069
floriansaubin <saubinflorian@gmail.com>
2022-03-11 10:53:08 +0100

    fix v1

--- server/index.js
+++ server/index.js
@@ -58,5 +58,5 @@ app.get('*', (req, res) => {
 });
 
 app.listen(process.env.PORT || 5000, () => {
-  console.log(`Server is runnin' on PORT: ${PORT}`)
+  console.log(`Server is runnin' on PORT: 5000`)
 })
